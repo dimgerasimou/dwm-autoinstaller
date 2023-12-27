90044fdc42d8dd71194381486c89c346d3394961
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-12-27 20:21:20 +0200

    Basic copy

---
+++ optimus-manager-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cp optimus-manager.conf /etc/optimus-manager/optimus-manager.conf
---
+++ zsh-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Spaceship prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
